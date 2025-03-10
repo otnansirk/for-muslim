596c71da51e803c6bc57828022338abc76434aa8
otnansirk <iam.otnansirk@gmail.com>
2025-03-10 22:48:24 +0700

    feat : home page;header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,30 @@
+import './app.css'
+import Icon from './component/Icon'
+
 function App() {
 
     return (
         <>
-            <h1>For Muslim</h1>
+            <div className='header'>
+                <div className='header-upcoming'>
+                    <div className='upcoming'>
+                        <div className='prayer'>Isha</div>
+                        <div className='time'>07:00 <span>pm</span></div>
+                        <span><img src={'./asset/plus-minus.svg'} /> in 2 hours 34 minutes</span>
+                    </div>
+                    <div className='location'>
+                        <div className='icon'><img src={'./asset/edit.svg'} /></div>
+                        <div className='city'>Ponorogo</div>
+                        <div className='country'>Indonesia</div>
+                    </div>
+                </div>
+                <div className='calculation-method'>
+                    <div>Kementerian Agama Republik Indonesia</div>
+                    <div><img src={'./asset/dropdown.svg'} /></div>
+                </div>
+                <div className='settings'><Icon icon='gear' /></div>
+                <div className='date'></div>
+            </div>
         </>
     )
 }
