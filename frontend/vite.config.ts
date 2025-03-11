abd561f4e22d28f9e6ec1746c2ed1b54d9572bb6
otnansirk <iam.otnansirk@gmail.com>
2025-03-11 13:35:44 +0700

    fix : update config file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import webExtension from 'vite-plugin-web-extension'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), webExtension({ manifest: 'manifest.json' })],
 })
