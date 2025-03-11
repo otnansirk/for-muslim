1343da65660cbdf0d2dcb6cd4da5ecb75fa635be
otnansirk <iam.otnansirk@gmail.com>
2025-03-11 10:27:05 +0700

    feat : body prayer time

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,59 @@
 import './app.css'
 import Icon from './component/Icon'
 
+const prayTime = [
+    {
+        id: "imsak",
+        icon: "imsak",
+        title: "Imsak",
+        time: "04:30 am",
+        notify: "off",
+        upcoming: false
+    },
+    {
+        id: "sholat-fajr",
+        icon: "sholat-fajr",
+        title: "Fajr",
+        time: "04:30 am",
+        notify: "off",
+        upcoming: false
+    },
+    {
+        id: "sholat-dhuhr",
+        icon: "sholat-dhuhr",
+        title: "Dhuhr",
+        time: "04:30 am",
+        notify: "off",
+        upcoming: false
+    },
+    {
+        id: "sholat-asr",
+        icon: "sholat-asr",
+        title: "Asr",
+        time: "04:30 am",
+        notify: "off",
+        upcoming: false
+    },
+    {
+        id: "sholat-maghrib",
+        icon: "sholat-maghrib",
+        title: "Maghrib",
+        time: "04:30 am",
+        notify: "off",
+        upcoming: false
+    },
+    {
+        id: "sholat-isha",
+        icon: "sholat-isha",
+        title: "Isha",
+        time: "04:30 am",
+        notify: "on",
+        upcoming: true
+    },
+]
+
 function App() {
+
 
     return (
         <>
@@ -23,7 +75,31 @@ function App() {
                     <div><img src={'./asset/dropdown.svg'} /></div>
                 </div>
                 <div className='settings'><Icon icon='gear' /></div>
-                <div className='date'></div>
+                <div className='date'>
+                    <div className='hijri'>1 Ramadhan 1446</div>
+                    <div className='geor'>1 March 2025</div>
+                </div>
+            </div>
+            <div className='body'>
+                <div className='table'>
+                    <div className='table-header'>
+                        <div className='table-tr'>
+                            <div className='table-td'>Prayer</div>
+                            <div className='table-td'>Time</div>
+                        </div>
+                    </div>
+                    <div className='table-body'>
+                        {
+                            prayTime.map((item, key) => (
+                                <div className={`table-tr ${item.upcoming && 'active'}`} key={key}>
+                                    <div className='table-td icon pray-icon'><Icon icon={item.icon} />{item.title}</div>
+                                    <div className='table-td'>{item.time}</div>
+                                    <div className='table-td icon notify-icon'><Icon icon={`notify-${item.notify}`} /></div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
             </div>
         </>
     )
