d2f1cd2ea1a9a1f24a77f3a6fcfbba9a4660529d
otnansirk <iam.otnansirk@gmail.com>
2025-03-16 14:48:36 +0700

    feat : weather

---
+++ frontend/src/component/pages/weather/Weather.tsx
@@ -0,0 +1,13 @@
+import Icon from '../../Icon'
+import './style.css'
+
+const Weather = () => {
+    return (
+        <div className='weather'>
+            <Icon icon='weather' />
+            Cloudy, 23&#xb0;
+        </div>
+    )
+}
+
+export default Weather
