9196214227b1760e89cfd55d27e5a5ac99ba6bfd
otnansirk <iam.otnansirk@gmail.com>
2025-03-17 06:52:43 +0700

    feat : weather

--- frontend/src/component/pages/weather/Weather.tsx
+++ frontend/src/component/pages/weather/Weather.tsx
@@ -2,11 +2,15 @@ import Icon from '../../Icon'
 import './style.css'
 
 const Weather = () => {
-    return (
-        <div className='weather'>
+    return (<div className='weather'>
+        <div className='temperature'>
             <Icon icon='weather' />
             Cloudy, 23&#xb0;
         </div>
+        <div className='location'>
+            Ponorogo, Indonesia
+        </div>
+    </div>
     )
 }
 
