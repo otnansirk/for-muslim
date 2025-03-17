aea9982da57669a38680e1333bb4fa2c7b38966b
otnansirk <iam.otnansirk@gmail.com>
2025-03-17 08:28:28 +0700

    feat : quotes component

---
+++ frontend/src/component/pages/quotes/Quote.tsx
@@ -0,0 +1,9 @@
+import './style.css'
+
+const Quote = () => {
+    return <div className='quotes'>
+        "dan siapa pun yang takut akan Allah - Dia akan menjadikannya jalan keluar dan akan menyediakan baginya dari tempat yang tidak dia harapkan. Dan siapa pun yang mengandalkan Allah - maka Dia cukup untuknya. Memang, Allah akan mencapai tujuan -Nya. Allah telah menetapkan untuk segala sesuatu yang ditetapkan."
+    </div>
+}
+
+export default Quote
