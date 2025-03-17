391eb5a11001eecab591b438ecf174aac31a9023
otnansirk <iam.otnansirk@gmail.com>
2025-03-17 20:39:20 +0700

    feat : dotenv and fix storage

---
+++ frontend/src/utils/Dotenv.ts
@@ -0,0 +1,5 @@
+const Dotenv = {
+    api_url: import.meta.env.VITE_API_URL || "",
+    api_key: import.meta.env.VITE_API_KEY || ""
+}
+export default Dotenv
--- frontend/src/utils/Storage.ts
+++ frontend/src/utils/Storage.ts
@@ -1,10 +1,15 @@
-export type ProfileProps = {
+export type StorageType = {
+    name?: string
     lat?: string
     lng?: string
     address?: string
     city?: string
     country?: string
     lang?: string
+    weather?: {
+        text?: string
+        temp?: number
+    }
     fajr?: {
         notify?: string
         time?: string
@@ -30,25 +35,26 @@ export type ProfileProps = {
 
 
 class Storage {
-    constructor() {
-        //
-    }
-
-    static profile = {
-        set: (value: ProfileProps) => {
+    static sync = {
+        set: <K extends keyof StorageType>(key: K, value: StorageType[K]) => {
             if (typeof chrome !== "undefined" && chrome.storage) {
-                chrome.storage.sync.get('profile').then(item => {
-                    chrome.storage.sync.set({ profile: { ...item.profile, ...value } });
+                chrome.storage.sync.get(key).then(item => {
+                    if (typeof value === 'object' && value != null) {
+                        const safeValue = JSON.parse(JSON.stringify(value));
+                        chrome.storage.sync.set({ [key]: { ...(item || {}), ...safeValue } });
+                    } else {
+                        chrome.storage.sync.set({ [key]: value });
+                    }
                 })
             } else {
                 console.error("Chrome Storage API is not available.");
             }
         },
-        get: async (key?: string): Promise<ProfileProps | undefined> => {
+        get: async (key: string): Promise<StorageType | undefined> => {
             if (typeof chrome !== "undefined" && chrome.storage) {
                 return new Promise((resolve) => {
-                    chrome.storage.sync.get('profile', result => {
-                        resolve(key ? result['profile'][key] : result['profile'])
+                    chrome.storage.sync.get(key, result => {
+                        resolve(result)
                     })
                 })
             } else {
@@ -56,7 +62,7 @@ class Storage {
                 return undefined;
             }
         },
-        listen: (callback: (item: ProfileProps) => void) => {
+        listen: (callback: (item: StorageType) => void) => {
             chrome.storage.onChanged.addListener(item => callback(item?.profile?.newValue))
         }
     }
