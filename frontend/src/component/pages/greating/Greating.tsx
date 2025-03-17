534e4bebe9a1bc484d2f3d41612fb19b30fbdbfc
otnansirk <iam.otnansirk@gmail.com>
2025-03-17 08:27:47 +0700

    feat : greating component

---
+++ frontend/src/component/pages/greating/Greating.tsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+const Greating = () => {
+    return (
+        <div className="greating">
+            Good Morning, Krisnanto
+        </div>
+    )
+}
+
+export default Greating
