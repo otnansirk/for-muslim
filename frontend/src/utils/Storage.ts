c11a2279e2ef1871c10f97980b7a470db1c21006
otnansirk <iam.otnansirk@gmail.com>
2025-03-19 20:13:50 +0700

    fix : adjustment Storage utils

--- frontend/src/utils/Storage.ts
+++ frontend/src/utils/Storage.ts
@@ -1,81 +1,59 @@
-export type WeatherType = {
-    text?: string
-    temp_c?: number
-    temp_f?: number
-    feels_c?: number
-    feels_f?: number
-    last_update?: number
-}
-
-export type StorageType = {
-    name?: string
-    lat?: string
-    lng?: string
-    address?: string
-    city?: string
-    country?: string
-    region?: string
-    lang?: string
-    weather?: WeatherType
-    hijri?: string
-    imsak?: {
-        notify?: string
-        time?: string
-    }
-    fajr?: {
-        notify?: string
-        time?: string
-    }
-    dhuhr?: {
-        notify?: string
-        time?: string
-    }
-    asr?: {
-        notify?: string
-        time?: string
-    }
-    maghrib?: {
-        notify?: string
-        time?: string
-    }
-    isha?: {
-        notify?: string
-        time?: string
-    }
-}
-
+import { StorageType } from "../types/Storage";
 
 class Storage {
-    static sync = {
-        set: <K extends keyof StorageType>(key: K, value: StorageType[K]) => {
-            if (typeof chrome !== "undefined" && chrome.storage) {
-                chrome.storage.sync.get(key).then(item => {
-                    if (typeof value === 'object' && value != null && typeof item === 'object' && item != null) {
-                        const safeValue = JSON.parse(JSON.stringify(value));
-                        chrome.storage.sync.set({ [key]: { ...item[key], ...safeValue } });
-                    } else {
-                        chrome.storage.sync.set({ [key]: value });
-                    }
-                })
+
+    private static deepMerge = <T extends StorageType>(target: T, source: Partial<T>): T => {
+        const result: T = { ...target };
+
+        for (const key in source) {
+            if (
+                source[key] !== null &&
+                typeof source[key] === "object" &&
+                !Array.isArray(source[key])
+            ) {
+                result[key] = this.deepMerge(target[key] ?? {}, source[key] as StorageType) as T[typeof key];
             } else {
-                console.error("Chrome Storage API is not available.");
+                result[key] = source[key] as T[Extract<keyof T, string>];
             }
-        },
-        get: async (key: string): Promise<StorageType | undefined> => {
-            if (typeof chrome !== "undefined" && chrome.storage) {
-                return new Promise((resolve) => {
-                    chrome.storage.sync.get(key, result => {
-                        resolve(result[key])
-                    })
-                })
-            } else {
-                console.error("Chrome Storage API is not available.");
-                return undefined;
-            }
-        },
-        listen: (key: string, callback: (item: StorageType) => void) => {
-            chrome.storage.onChanged.addListener(item => callback(item?.[key]?.newValue))
         }
+
+        return result;
+    };
+
+    private static set = <K extends keyof StorageType>(key: K, value: StorageType[K]) => {
+        if (typeof chrome !== "undefined" && chrome.storage) {
+            chrome.storage.sync.get(key).then(item => {
+
+                const oldData = (item[key] ?? {}) as Partial<StorageType[K]>;
+                const updatedData = this.deepMerge(oldData, value);
+                chrome.storage.sync.set({ [key]: updatedData });
+
+            })
+        } else {
+            console.error("Chrome Storage API is not available.");
+        }
+    }
+
+    private static get = async <K extends keyof StorageType>(key: K, callback: <T>(item: T) => void) => {
+        if (typeof chrome !== "undefined" && chrome.storage) {
+            chrome.storage.sync.get(key, result => callback(result[key]))
+        } else {
+            console.error("Chrome Storage API is not available.");
+        }
+    }
+
+    private static listen = (key: string, callback: <T>(item: T) => void) => {
+        if (typeof chrome !== "undefined" && chrome.storage) {
+            chrome.storage.onChanged.addListener(item => callback(item?.[key]?.newValue))
+        } else {
+            console.error("Chrome Storage API is not available.");
+        }
+    }
+
+    static sync = {
+        set: this.set,
+        get: this.get,
+        listen: this.listen
     }
 }
 
