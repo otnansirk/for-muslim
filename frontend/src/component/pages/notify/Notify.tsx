a3403d4598ba742c144f3aabbf2eeb2bd0f6e63f
otnansirk <iam.otnansirk@gmail.com>
2025-03-21 23:43:25 +0700

    feat : notify component

---
+++ frontend/src/component/pages/notify/Notify.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import Icon from '../../Icon'
+import './style.css'
+
+const Notify = () => {
+    const [active, setActive] = useState<string>("false")
+    const onclose = () => {
+        setActive("")
+    }
+
+    return (
+        <div className={`notify ${active}`} onClick={onclose}>
+            <div className='content'>
+                <Icon icon="logo" />
+                <div className='text'>
+                    <div className='title'>It's time for Isha prayer <strong>Isha!</strong></div>
+                    <div className='desc'>Luangkan waktu sejenak untuk beribadah.</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Notify
