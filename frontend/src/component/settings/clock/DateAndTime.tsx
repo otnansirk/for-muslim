8ba864bfd2e87756afc0ff0c2f601447571ff7b0
otnansirk <iam.otnansirk@gmail.com>
2025-04-13 22:31:03 +0700

    feat : Date and Time settings

--- frontend/src/component/settings/clock/DateAndTime.tsx
+++ frontend/src/component/settings/clock/DateAndTime.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { DATE_FORMAT } from "../../../constant/date_and_time"
-import { DateType } from "../../../types/Storage"
+import { DateType, TimeType } from "../../../types/Storage"
 import Switch from "../../form/switch/Switch"
 import Select from "../../form/select/Select"
 import Storage from "../../../utils/Storage"
@@ -9,11 +9,27 @@ const DateAndTime = () => {
     const dateEnableRef = useRef<HTMLInputElement>(null)
     const dateFormatRef = useRef<HTMLSelectElement>(null)
 
+    const timeEnableRef = useRef<HTMLInputElement>(null)
+    const time24HoursRef = useRef<HTMLInputElement>(null)
+    const timeShowSecondsRef = useRef<HTMLInputElement>(null)
+    const timeShowAMPMRef = useRef<HTMLInputElement>(null)
+
+    const [showAMPM, setShowAMPM] = useState("hidden")
+
     useEffect(() => {
         Storage.sync.watch("date", (item) => {
             const data = item as DateType
             dateEnableRef.current!.checked = data?.enable ?? false
             dateFormatRef.current!.value = data?.format?.value ?? ""
+        })
+
+        Storage.sync.watch("time", (item) => {
+            const data = item as TimeType
+            timeEnableRef.current!.checked = data?.enable ?? false
+            time24HoursRef.current!.checked = data?.is_24_hours ?? false
+            timeShowSecondsRef.current!.checked = data?.show_seconds ?? false
+            timeShowAMPMRef.current!.checked = data?.show_ampm ?? false
+            setShowAMPM(data?.is_24_hours ? "hidden" : "")
         })
     }, [])
 
@@ -27,7 +43,6 @@ const DateAndTime = () => {
                     Enable Date
                 </div>
                 <Switch
-                    isChecked={false}
                     ref={dateEnableRef}
                     onChange={e => Storage.sync.set('date', { enable: e.target.checked })}
                 />
@@ -44,6 +59,51 @@ const DateAndTime = () => {
                 />
             </div>
         </div>
+
+        {/* TIME SETTINGS */}
+        <div className='settings-items'>
+            <div className='items'>
+                <div className='items-title'>
+                    Enable Time
+                </div>
+                <Switch
+                    ref={timeEnableRef}
+                    onChange={e => Storage.sync.set('time', { enable: e.target.checked })}
+                />
+            </div>
+            <hr />
+            <div className='items'>
+                <div className='items-title'>
+                    24-Hours
+                </div>
+                <Switch
+                    ref={time24HoursRef}
+                    onChange={e => Storage.sync.set('time', { is_24_hours: e.target.checked })}
+                />
+            </div>
+            <div className={`dropshow ${showAMPM}`}>
+                <hr />
+                <div className='items'>
+                    <div className='items-title'>
+                        Show AM/PM
+                    </div>
+                    <Switch
+                        ref={timeShowAMPMRef}
+                        onChange={e => Storage.sync.set('time', { show_ampm: e.target.checked })}
+                    />
+                </div>
+            </div>
+            <hr />
+            <div className='items'>
+                <div className='items-title'>
+                    Show Seconds
+                </div>
+                <Switch
+                    ref={timeShowSecondsRef}
+                    onChange={e => Storage.sync.set('time', { show_seconds: e.target.checked })}
+                />
+            </div>
+        </div >
     </>
 }
 
--- frontend/src/types/Storage.ts
+++ frontend/src/types/Storage.ts
@@ -74,7 +74,7 @@ export type DateType = {
 
 export type TimeType = {
     enable?: boolean
-    format?: "12" | "24"
-    ampm?: boolean
+    is_24_hours?: boolean
+    show_ampm?: boolean
     show_seconds?: boolean
 }
