1bbe4e2674b79c60a1d2e02158d71cf9b9207729
otnansirk <iam.otnansirk@gmail.com>
2025-04-13 22:30:19 +0700

    fix : style and isChecked

--- frontend/src/component/form/switch/Switch.tsx
+++ frontend/src/component/form/switch/Switch.tsx
@@ -4,12 +4,13 @@ import './style.css'
 
 
 type SwitchProps = {
-    isChecked: boolean
+    isChecked?: boolean
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
     ref?: RefObject<HTMLInputElement | null>
 }
 
 const Switch = ({ onChange, isChecked, ref }: SwitchProps) => {
+
     return <label className='switch-input'>
         <input type='checkbox' defaultChecked={isChecked} onChange={onChange} ref={ref} />
         <div className='switch' />
