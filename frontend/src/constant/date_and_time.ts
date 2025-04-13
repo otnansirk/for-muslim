596022e65942589cf7f3e24078e67c86305e7acf
otnansirk <iam.otnansirk@gmail.com>
2025-04-13 18:26:18 +0700

    feat : DATE format options

--- frontend/src/constant/date_and_time.ts
+++ frontend/src/constant/date_and_time.ts
@@ -1,10 +1,45 @@
+import { ConfigDateTypes } from "../types/datetime";
+
 export const DATE_FORMAT = [
     {
-        label: "Sunday, February 01",
-        value: "day_long, month_long day"
+        label: "Sunday, February 30",
+        value: "weekday, month day",
+        config: {
+            day: "2-digit",
+            month: "long",
+        } as ConfigDateTypes
     },
     {
-        label: "Sunday, 01 February 2025",
-        value: "day, day month_long year_long"
+        label: "Sunday, 30 February 2025",
+        value: "weekday, day month year",
+        config: {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        } as ConfigDateTypes
+    },
+    {
+        label: "Sunday 30",
+        value: "weekday day",
+        config: {
+            day: "2-digit",
+            month: "long",
+        } as ConfigDateTypes
+    },
+    {
+        label: "30 Frebruary 2025",
+        value: "day month year",
+        config: {
+            day: "2-digit",
+            month: "long",
+        } as ConfigDateTypes
+    },
+    {
+        label: "Sunday 30 Frebruary",
+        value: "weekday day month",
+        config: {
+            day: "2-digit",
+            month: "long",
+        } as ConfigDateTypes
     },
 ]
