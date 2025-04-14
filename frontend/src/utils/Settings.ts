4ada57d801da9d2affed43c46e29ba65a4087c14
otnansirk <iam.otnansirk@gmail.com>
2025-04-14 20:55:12 +0700

    feat : default settings

---
+++ frontend/src/utils/Settings.ts
@@ -0,0 +1,22 @@
+export const defaultSettings = {
+    "date": {
+        "enable": true,
+        "format": {
+            "config": {
+                "day": "2-digit",
+                "month": "long"
+            },
+            "label": "Sunday, February 30",
+            "value": "weekday, month day"
+        }
+    },
+    "greating": {
+        "enable": true
+    },
+    "time": {
+        "enable": true,
+        "hour12": true,
+        "show_ampm": true,
+        "show_seconds": false
+    }
+}
