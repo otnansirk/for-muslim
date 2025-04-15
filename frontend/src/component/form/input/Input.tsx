9a9e6d00800e250da9e29475637c43cfc80316ef
otnansirk <iam.otnansirk@gmail.com>
2025-04-15 21:28:47 +0700

    fix : add datalist to input

--- frontend/src/component/form/input/Input.tsx
+++ frontend/src/component/form/input/Input.tsx
@@ -1,13 +1,30 @@
 import { RefObject } from 'react'
 import './style.css'
+import Each from '../../Each'
 
 type InputProps = {
     value?: string
+    datalist?: {
+        value: string
+        label: string
+    }[]
     placeholder?: string
     ref?: RefObject<HTMLInputElement | null>
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const Input = ({ onChange, value, ref, placeholder }: InputProps) => <input type='text' className='form-items' onChange={onChange} defaultValue={value} ref={ref} placeholder={placeholder} />
+const Input = ({ onChange, value, ref, placeholder, datalist }: InputProps) => {
+    return <>
+        <input type='text' list='fm_input-list' className='form-items' onChange={onChange} defaultValue={value} ref={ref} placeholder={placeholder} />
+        {datalist &&
+            <datalist id='fm_input-list'>
+                <Each
+                    data={datalist}
+                    render={(item, key) => <option key={key} value={item.value}>{item.label}</option>}
+                />
+            </datalist>
+        }
+    </>
+}
 
 export default Input
