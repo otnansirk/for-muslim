bc817c023d31bb3e26da66daaf69644c6252d640
otnansirk <iam.otnansirk@gmail.com>
2025-04-18 05:36:08 +0700

    fix : Icon component to load svg as component

--- frontend/src/component/Icon.tsx
+++ frontend/src/component/Icon.tsx
@@ -1,10 +1,25 @@
+import './icon.css';
+
 type IconProps = {
     icon: string;
     className?: string;
 };
 
-const Icon: React.FC<IconProps> = ({ icon, className }) => (
-    <img className={className} src={chrome.runtime.getURL(`./assets/${icon}.svg`)} />
-)
+const icons = import.meta.glob('../assets/icons/*.svg', {
+    eager: true,
+    query: 'react',
+    import: 'default'
+}) as Record<string, React.FC<React.SVGProps<SVGSVGElement>>>
 
-export default Icon
+const Icon: React.FC<IconProps> = ({ icon, className = '' }) => {
+    const iconKey = `../assets/icons/${icon}.svg`;
+    const SVGIcon = icons[iconKey];
+
+    if (!SVGIcon) {
+        console.warn(`Icon "${icon}" not found`);
+        return null;
+    }
+    return <SVGIcon className={`icon ${icon}-icon ${className}`} />
+};
+
+export default Icon;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import webExtension from 'vite-plugin-web-extension'
+import svgr from 'vite-plugin-svgr'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), webExtension({ manifest: 'manifest.json' })],
+    plugins: [svgr(), react(), webExtension({ manifest: 'manifest.json' })],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, '/src'),
+        }
+    }
 })
