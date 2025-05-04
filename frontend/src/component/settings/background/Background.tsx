c3dd6ea9d3d1075af1fb12c3c6363e9b922f5bae
otnansirk <iam.otnansirk@gmail.com>
2025-05-04 10:33:57 +0700

    fix : background loader setting;improve performace

--- frontend/src/component/settings/background/Background.tsx
+++ frontend/src/component/settings/background/Background.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useRef, useState } from "react"
-import { BACKGROUND_COLLECTION_ISLAMIC, BACKGROUND_COLLECTIONS, BACKGROUND_REFRESH_FREQUENCY, BACKGROUND_TYPES } from "../../../constant/background"
-import { fetchDataUnsplash, refreshBackgroundHandler } from "../../../utils/Background"
-import { BackgroundType, UnsplashType } from "../../../types/Storage"
+import { BACKGROUND_COLLECTION, BACKGROUND_COLLECTIONS, BACKGROUND_REFRESH_FREQUENCY, BACKGROUND_TYPES } from "../../../constant/background"
+import { BackgroundType } from "../../../types/Storage"
 import Select from "../../form/select/Select"
 import Storage from "../../../utils/Storage"
+import Input from "../../form/input/Input"
 import Icon from "../../Icon"
 
 import "./style.css"
@@ -11,39 +11,70 @@ import Loader from "../../loader/Loader"
 
 
 const Background = () => {
-    const rotateRef = useRef<number>(0)
 
-    const isTypeRef = useRef<HTMLSelectElement>(null)
-    const isRefreshFrequencyRef = useRef<HTMLSelectElement>(null)
-    const isRefreshIconRef = useRef<SVGSVGElement>(null)
-    const collectionsRef = useRef<HTMLSelectElement>(null)
+    const typeRef = useRef<HTMLSelectElement>(null)
+    const frequencyRef = useRef<HTMLSelectElement>(null)
+    const refreshIconRef = useRef<SVGSVGElement>(null)
+    const collectionTypeRef = useRef<HTMLSelectElement>(null)
+    const collectionValueRef = useRef<HTMLInputElement>(null)
+    const lastCollectionValueRef = useRef<string>("")
 
-    const [loading, setLoading] = useState(false)
+    const [collectionType, setCollectionType] = useState<string>()
+    const [onRefreshBackground, setOnRefreshBackground] = useState<boolean>(false)
+    const [onChangeUnsplashCollection, setOnChangeUnsplashCollection] = useState<boolean | undefined>(undefined)
+    const [currentCollectionValue, setCurrentCollectionValue] = useState<string>("")
+
+    const onCollectionHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = e.target.value
+        setCollectionType(value)
+        if (value !== "custom") {
+            setOnChangeUnsplashCollection(true)
+            Storage.local.set("onChangeUnsplashCollection", { collection_type: value, collection_value: BACKGROUND_COLLECTION[value as string] })
+            Storage.sync.set("background", { collection_type: value, collection_value: BACKGROUND_COLLECTION[value as string] })
+        }
+    }
+
+    const onSaveCustomCollection = () => {
+        if (onChangeUnsplashCollection) return
+        setOnChangeUnsplashCollection(true)
+        Storage.local.set("onChangeUnsplashCollection", { type: "custom", value: collectionValueRef.current?.value })
+        Storage.sync.set("background", { collection_type: "custom", collection_value: collectionValueRef.current?.value })
+    }
 
     useEffect(() => {
-
-        Storage.sync.get("background", async (item) => {
+        Storage.sync.get("background", item => {
             const bg = item as BackgroundType
-            isRefreshFrequencyRef.current!.value = bg?.frequency ?? "tab"
-            isTypeRef.current!.value = bg?.type ?? "unsplash"
-            collectionsRef.current!.value = bg?.collections ?? BACKGROUND_COLLECTION_ISLAMIC
+            if (bg) {
+                typeRef.current!.value = bg.type as string
+                frequencyRef.current!.value = bg.frequency as string
+                collectionTypeRef.current!.value = bg.collection_type as string
+                collectionValueRef.current!.placeholder = bg.collection_value as string
+                lastCollectionValueRef.current = bg.collection_value as string
 
-            Storage.local.get("unsplash", async (cache) => {
-                const data = cache as UnsplashType[]
-                if (!Array.isArray(data) || data.length <= 2) {
-                    await fetchDataUnsplash(data, { collections: bg.collections })
-                    Storage.sync.set("background", { frequency: "tab", type: "unsplash" })
-                    return
-                }
-            })
+                setCollectionType(bg.collection_type as string)
+                setCurrentCollectionValue(bg.collection_value as string)
+            }
         })
 
+        Storage.local.watch("onRefreshBackground", (load) => {
+            setTimeout(() => {
+                setOnRefreshBackground(load as boolean)
+            }, 500);
+        })
+
+        Storage.local.watch("onChangeUnsplashCollection", (load) => {
+            if (collectionValueRef.current?.value && load === false) {
+                setTimeout(() => {
+                    setOnChangeUnsplashCollection(false)
+                    lastCollectionValueRef.current = collectionValueRef.current?.value as string
+                }, 500);
+            }
+        })
     }, [])
 
-
-    return <div className={`background-settings ${loading && 'loading'}`}>
+    return <div className={`background-settings`}>
         <h2 className='settings-title'>
-            {loading && <Loader />} BACKGROUND
+            BACKGROUND
         </h2>
         <div className='settings-items'>
             <div className='items'>
@@ -52,7 +83,7 @@ const Background = () => {
                 </div>
                 <Select
                     items={BACKGROUND_TYPES}
-                    ref={isTypeRef}
+                    ref={typeRef}
                     onSelect={e => Storage.sync.set('background', { type: e.target.value })}
                 />
             </div>
@@ -64,19 +95,20 @@ const Background = () => {
                 <div className="items-content">
                     <div
                         className={`save-action`}
-                        onClick={async () => {
-                            isRefreshIconRef.current!.style.transform = `rotate(${rotateRef.current += 360}deg)`
-                            await refreshBackgroundHandler()
+                        onClick={() => {
+                            setOnRefreshBackground(true)
+                            if (!onRefreshBackground) {
+                                Storage.local.set("onRefreshBackground", true)
+                            }
                         }}
                     >
-                        <Icon ref={isRefreshIconRef} icon="arrow-path" className="refresh-icon" />
+                        <Icon ref={refreshIconRef} icon="arrow-path" className={`refresh-icon ${onRefreshBackground && 'spin'}`} />
                     </div>
                     <Select
                         items={BACKGROUND_REFRESH_FREQUENCY}
-                        ref={isRefreshFrequencyRef}
-                        onSelect={(e) => {
-                            const data: BackgroundType = { frequency: e.target.value }
-                            Storage.sync.set('background', data)
+                        ref={frequencyRef}
+                        onSelect={() => {
+
                         }}
                     />
                 </div>
@@ -89,14 +121,36 @@ const Background = () => {
                 <div className="items-content">
                     <Select
                         items={BACKGROUND_COLLECTIONS}
-                        ref={collectionsRef}
-                        onSelect={async (e) => {
-                            setLoading(true)
-                            Storage.sync.set('background', { collections: e.target.value })
-                            await fetchDataUnsplash([], { collections: e.target.value })
-                            setLoading(false)
-                        }}
+                        ref={collectionTypeRef}
+                        onSelect={onCollectionHandler}
                     />
+                </div>
+            </div>
+            <div className={`dropshow ${collectionType !== "custom" && "hidden"}`}>
+                <hr />
+                <div className='items'>
+                    <div className='items-title'>
+                        Custom
+                    </div>
+                    <div className="items-content">
+                        {
+                            (currentCollectionValue && currentCollectionValue !== lastCollectionValueRef.current) &&
+                            <div
+                                className={`save-action`}
+                                onClick={onSaveCustomCollection}
+                            >
+                                {
+                                    onChangeUnsplashCollection
+                                        ? <Loader />
+                                        : <Icon icon="check" className="check-icon" />
+                                }
+                            </div>
+                        }
+                        <Input
+                            ref={collectionValueRef}
+                            onChange={e => setCurrentCollectionValue(e.target.value)}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
