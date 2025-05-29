b0c6f4c2c7fc84235f60df47c249c1b452a9dcc8
otnansirk <iam.otnansirk@gmail.com>
2025-05-29 14:00:11 +0700

    fix : get store default value from default settings

--- frontend/src/utils/Store.ts
+++ frontend/src/utils/Store.ts
@@ -1,6 +1,8 @@
 import { create } from "zustand";
 import { GreetingsStoreType, BackgroundStoreType, TimeStoreType } from "../types/Store";
 import Storage from "./Storage";
+import { getDefaultSetting } from "./Settings";
+import { BACKGROUND_BLUR_INTENSITY, BACKGROUND_BRIGHTNESS, GREETING_FONT_SIZE, GREETING_TEXT_SHADOW, TIME_FONT_SIZE } from "../constant/settings";
 
 
 let timeoutRef: number | null = null
@@ -13,13 +15,13 @@ const saveToStorage = (callback: () => void) => {
 }
 
 export const useGreetingsStore = create<GreetingsStoreType>((set) => ({
-    textShadow: 20,
+    textShadow: GREETING_FONT_SIZE,
     setTextShadow: (textShadow: number) => {
         saveToStorage(() => Storage.sync.set("greeting", { text_shadow: textShadow }))
         return set(() => ({ textShadow }))
     },
 
-    fontSize: 45,
+    fontSize: GREETING_TEXT_SHADOW,
     setFontSize: (fontSize: number) => {
         saveToStorage(() => Storage.sync.set("greeting", { font_size: fontSize }))
         return set(() => ({ fontSize }))
@@ -29,13 +31,13 @@ export const useGreetingsStore = create<GreetingsStoreType>((set) => ({
 }))
 
 export const useBackgroundStore = create<BackgroundStoreType>((set) => ({
-    blurIntensity: 0,
+    blurIntensity: BACKGROUND_BLUR_INTENSITY,
     setBlurIntensity: (blurIntensity: number) => {
         saveToStorage(() => Storage.sync.set("background", { blur_intensity: blurIntensity }))
         return set(() => ({ blurIntensity }))
     },
 
-    brightness: 0,
+    brightness: BACKGROUND_BRIGHTNESS,
     setBrightness: (brightness: number) => {
         saveToStorage(() => Storage.sync.set("background", { brightness }))
         return set(() => ({ brightness }))
@@ -46,7 +48,7 @@ export const useBackgroundStore = create<BackgroundStoreType>((set) => ({
 
 export const useTimeStore = create<TimeStoreType>((set) => ({
 
-    fontSize: 65,
+    fontSize: TIME_FONT_SIZE,
     setFontSize: (fontSize: number) => {
         saveToStorage(() => Storage.sync.set("time", { font_size: fontSize }))
         return set(() => ({ fontSize }))
